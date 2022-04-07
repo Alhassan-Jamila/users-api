9b96dbcda2c8b6f6dd6aa19ff80e46d40c8eeffc
Alhassan-Jamila <jamilaal865@gmail.com>
2022-04-07 15:09:02 +0000

    first commit

---
+++ data/users.js
@@ -0,0 +1,8 @@
+const users=[
+    {id:1, name:"Joy", age:25},
+    {id:2, name:"Mary", age:22},
+    {id:3, name:"Esi", age:28},
+    {id:4, name:"Ami", age:26},
+    {id:5, name:"Cj", age:21},
+]
+module.exports=users
